97c011875487209903a67c41abba38e435ae3776
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-15 21:50:14 +0100

    project set-up

---
+++ app/src/main/java/com/example/myapplication/Recipes.java
@@ -0,0 +1,12 @@
+package com.example.myapplication;
+
+public class Recipes {
+    private String RecipeName;
+    private String RecipeIngredient;
+    private String RecipeMethodTitle;
+    private String Recipe;
+    private int Thumbnail;
+
+
+
+}
