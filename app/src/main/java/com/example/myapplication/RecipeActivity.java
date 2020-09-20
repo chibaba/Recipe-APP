317673a2ff3078165218f1943c778adaf1bdf4ec
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-20 16:49:36 +0100

    wip

--- app/src/main/java/com/example/myapplication/RecipeActivity.java
+++ app/src/main/java/com/example/myapplication/RecipeActivity.java
@@ -2,13 +2,29 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RecipeActivity extends AppCompatActivity {
+
+    private TextView mRecipeName;
+    private TextView mRecipeIngredients;
+    private TextView mRecipeMethodTitle;
+    private TextView mRecipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe2);
+
+
+        mRecipeName = (TextView)findViewById(R.id.text_recipe);
+        mRecipeIngredients = (TextView)findViewById(R.id.ingredients);
+        mRecipeMethodTitle = (TextView)findViewById(R.id.method);
+        mRecipe= (TextView)findViewById(R.id.recipe);
+
+        Intent intent = getIntent();
+        String Title = intent.getExtras().getString( "RecipeName");
     }
 }
