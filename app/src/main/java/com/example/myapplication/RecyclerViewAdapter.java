04ce1c0ee2a370c705b1f8231d6e282a12cf0aef
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-09-20 18:04:59 +0100

    still creating view adapter

--- app/src/main/java/com/example/myapplication/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/myapplication/RecyclerViewAdapter.java
@@ -2,8 +2,11 @@ package com.example.myapplication;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.MyHolder> {
@@ -24,8 +27,15 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
     }
 
     public class MyHolder extends RecyclerView.ViewHolder {
+
+        TextView recipeTitle;
+        CardView  cardView;
+        ImageView img_recipe_thumbnail;
         public MyHolder(@NonNull View itemView) {
             super(itemView);
+
+            recipeTitle = (TextView) itemView.findViewById(R.id.text_recipe);
+            img_recipe_thumbnail = (ImageView)itemView.findViewById(R.id.recipe_img_id);
         }
     }
 }
